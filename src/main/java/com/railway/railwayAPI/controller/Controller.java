323f9b4537311c755a5925a5b1e364db18687c82
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2023-12-29 18:36:07 +0530

    added new endpoint to check api status

---
+++ src/main/java/com/railway/railwayAPI/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.railway.railwayAPI.controller;
+
+import com.railway.railwayAPI.model.SearchInput;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+public class Controller {
+    @RequestMapping(value = "/status", method = RequestMethod.GET)
+    public ResponseEntity<?> getStatus() {
+        return ResponseEntity.ok().body(Map.of("status", "UP"));
+    }
+}
